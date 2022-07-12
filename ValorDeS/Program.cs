f564378aeb07b50a4eaeffcd2073004d27b47659
Evelin Raiane <evelin_raiane@hotmail.com>
2022-07-12 13:14:56 -0300

    adiciona projeto

---
+++ ValorDeS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ValorDeS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, s = 0;
+
+            for (a = 1; a <= 100; a++)
+            {
+                s+= 1 / a;
+            }
+
+            var x = Math.Round(s, 2);
+
+            Console.WriteLine(x);
+        }
+    }
+}
